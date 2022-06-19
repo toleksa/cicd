7b547fda178f45d5a95a6cc15fd0db75ea3e5a91
toleksa <toleksa@gmail.com>
2022-06-19 17:36:20 +0200

    init

---
+++ src/python-http/www/test_web.py
@@ -0,0 +1,14 @@
+import requests
+
+def test_server_is_alive():
+    response = requests.get("http://localhost:8888/")
+    assert response.status_code == 200
+
+def test_server_logo():
+    response = requests.get("http://localhost:8888/logo.png")
+    assert response.status_code == 200
+
+def test_server_404():
+    response = requests.get("http://localhost:8888/foobar")
+    assert response.status_code == 404
+
