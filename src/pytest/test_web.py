6bf2efc20f9b4fb17dc27ac861eb8c6fc8c8ac03
toleksa <toleksa@gmail.com>
2022-06-23 15:01:52 +0200

    debug

--- src/pytest/test_web.py
+++ src/pytest/test_web.py
@@ -1,14 +1,14 @@
 import requests
 
 def test_server_is_alive():
-    response = requests.get("http://webserver:8888/")
+    response = requests.get("http://webserver/")
     assert response.status_code == 200
 
 def test_server_logo():
-    response = requests.get("http://webserver:8888/logo.png")
+    response = requests.get("http://webserver/logo.png")
     assert response.status_code == 200
 
 def test_server_404():
-    response = requests.get("http://webserver:8888/foobar")
+    response = requests.get("http://webserver/foobar")
     assert response.status_code == 404
 
