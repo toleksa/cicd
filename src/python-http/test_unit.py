4321420fc76241d7b3305821d83489bcfa77caf0
toleksa <toleksa@gmail.com>
2022-07-07 09:21:11 +0200

    check logo

--- src/python-http/test_unit.py
+++ src/python-http/test_unit.py
@@ -2,5 +2,9 @@ import os
 
 def test_index():
     os.path.exists("www/index.html")
-    assert os.path.getsize("www/index.html") == 200
+    assert os.path.getsize("www/index.html") == 57
+
+def test_logo():
+    os.path.exists("www/logo.png")
+    assert os.path.getsize("www/logo.png") == 11543
 
