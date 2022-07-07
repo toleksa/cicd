2b6da58595266173df1d5e7152ddf559c4eb2647
toleksa <toleksa@gmail.com>
2022-07-07 09:52:09 +0200

    check www/

--- src/python-http/test_unit.py
+++ src/python-http/test_unit.py
@@ -1,5 +1,9 @@
 import os
 
+def test_www():
+    assert os.path.exists("www")
+    assert os.path.isdir("www")
+
 def test_index():
     os.path.exists("www/index.html")
     assert os.path.getsize("www/index.html") == 57
